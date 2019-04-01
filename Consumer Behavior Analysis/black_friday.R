966344684494d0b32e027f5dfe129687c6f40255
Das <soumya.das@fiskars.com>
2019-04-01 17:59:17 +0300

    01.04.2019

--- Consumer Behavior Analysis/black_friday.R
+++ Consumer Behavior Analysis/black_friday.R
@@ -1,10 +1,17 @@
 #reading the file from dell
-black_friday=read.csv("/home/soumya/Desktop/gitrepo/Business-Analytics/business_datasets/BlackFriday.csv")
+#black_friday=read.csv("/home/soumya/Desktop/gitrepo/Business-Analytics/business_datasets/BlackFriday.csv")
 #reading the file from lenovo
+black_friday=read.csv("C:/Users/sdas/Desktop/myprojects/Business-Analytics/business_datasets/BlackFriday.csv")
 
-
-#data exploration
+#######################data exploration#######################################
+#Unique values
 colnames(black_friday)#name of the columns
 length(unique(black_friday$User_ID)) #number of unique UserID
+print("Unique values for each feature:")
+
+for (f in 1:ncol(black_friday)) {
+  print(paste(colnames(black_friday[f]),":",length(unique(black_friday[,f])) ))
+}
 
 #Distribution
+
